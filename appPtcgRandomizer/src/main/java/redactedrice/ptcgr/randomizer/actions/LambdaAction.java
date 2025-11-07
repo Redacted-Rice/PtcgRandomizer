54d2d1eaba208e0c82917a72dd2ba1753b829d57
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2025-11-07 15:20:10 -0500

    First cut of moving to new randomizer. Got compiling at least

--- appPtcgRandomizer/src/main/java/redactedrice/ptcgr/randomizer/actions/LambdaAction.java
+++ appPtcgRandomizer/src/main/java/redactedrice/ptcgr/randomizer/actions/LambdaAction.java
@@ -3,32 +3,30 @@ package redactedrice.ptcgr.randomizer.actions;
 
 import redactedrice.ptcgr.rom.Rom;
 
-public class LambdaAction extends Action {
+public class LambdaAction {
 
     PerformLambda perform;
 
     public LambdaAction(String category, String name, String description, PerformLambda perform) {
-        super(category, name, description);
+        // super(category, name, description);
         this.perform = perform;
     }
 
     public LambdaAction(String category, StringLambda name, StringLambda description,
             PerformLambda perform) {
-        super(category, name, description);
+        // super(category, name, description);
         this.perform = perform;
     }
 
     protected LambdaAction(LambdaAction toCopy) {
-        super(toCopy);
+        // super(toCopy);
         this.perform = toCopy.perform;
     }
 
-    @Override
-    public Action copy() {
+    public LambdaAction copy() {
         return new LambdaAction(this);
     }
 
-    @Override
     public void perform(Rom rom) {
         perform.perform(rom);
     }
