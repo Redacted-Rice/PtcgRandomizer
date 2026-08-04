1047f7e818f887bb64fced5addcadb799147b080
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2026-08-04 09:22:03 -0400

    Adding a combo box for log level

--- appPtcgRandomizer/src/main/java/redactedrice/ptcgr/randomizer/Settings.java
+++ appPtcgRandomizer/src/main/java/redactedrice/ptcgr/randomizer/Settings.java
@@ -2,10 +2,13 @@ package redactedrice.ptcgr.randomizer;
 
 import java.util.Random;
 
+import redactedrice.randomizer.utils.LogLevel;
+
 public class Settings {
     private final Random rand = new Random();
     private String seed = String.valueOf(rand.nextInt());
     private boolean logDetails;
+    private LogLevel logLevel = LogLevel.INFO;
 
     public int getSeedValue() {
         try {
@@ -33,5 +36,13 @@ public class Settings {
 
     public void setLogDetails(boolean logDetails) {
         this.logDetails = logDetails;
+    }
+
+    public LogLevel getLogLevel() {
+        return logLevel;
+    }
+
+    public void setLogLevel(LogLevel logLevel) {
+        this.logLevel = logLevel != null ? logLevel : LogLevel.INFO;
     }
 }
